a08e5963e92504212c2447732e8d92cdd1f07a82
Lara Ramsey <lararams3y@gmail.com>
2023-10-18 22:29:01 -0700

    adding a counter

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 'use strict';
 
-console.log('hello kitty:');
+//console.log('hello kitty:');
 
 let userName = prompt('Hello. What is your name?');
 
-alert('Welcome to my site ' + userName + '. Let\'s play a guessing game. Please answer yes/no or y/n.');
+alert(`Welcome to my site ${userName}. Let's play a guessing game. Please answer yes/no or y/n.`);
+
+let correctCount = 0;
 
 //Q1
 let q1 = prompt ('Do I have a daughter?').toLowerCase();
 
 if(q1 === 'y' || q1 === 'yes'){
   alert('You are correct! Next question...');
+  correctCount += 1;
 } else if(q1 === 'n' || q1 === 'no'){
   alert('Wrong! I do have a daughter. She\'s fabulous! Next question...');
 }
@@ -26,6 +29,7 @@ let q2 = prompt ('Do I have a son?').toLowerCase();
 
 if(q2 === 'y' || q2 === 'yes'){
   alert('Correct again! Next question...');
+  correctCount += 1;
 } else if(q2 === 'n' || q2 === 'no'){
   alert('Fail! I do. He\'s a sweetie! Next question...');
 }
@@ -43,6 +47,7 @@ if(q3 === 'Y' || q3 === 'YES'){
   alert('Pppphhhhfffwahahahaha! No way!! Next question...');
 } else if( q3 === 'N' || q3 === 'NO'){
   alert('That\'s for darn certain. I\'m gonna leave that kind of child rearing to the great grandmothers. Next question...');
+  correctCount += 1;
 }
 
 //if(q3 === 'Y' || q3 === 'YES'){
@@ -56,6 +61,7 @@ let q4 = prompt ('Do I like Halloween?').toLowerCase();
 
 if (q4 === 'y' || q4 === 'yes'){
   alert('Totally! Next question...');
+  correctCount += 1;
 } else if(q4 === 'n' || q4 === 'no'){
   alert('Booo! You are soo wrong. Next question...');
 }
@@ -70,10 +76,13 @@ if (q4 === 'y' || q4 === 'yes'){
 let q5 = prompt ('Do I like Dia De Los Muertos?').toUpperCase();
 
 if (q5 === 'Y' || q5 === 'YES'){
-  alert('Si, es la verdad! It\'s my favorite!' + ' Thank you for playing this guessing game with me, ' + userName + '!');
+  alert('Si, es la verdad! It\'s my favorite!');
+  correctCount += 1;
 } else if(q5 === 'N' || q5 === 'NO'){
-  alert('Ai caramba, pobrecito! You are very mistaken.' + ' Thank you for playing this guessing game with me, ' + userName + '. Better luck next time! LOL!');
+  alert('Ai caramba, pobrecito! You are very mistaken. Better luck next time! LOL!');
 }
+
+alert(`Thank you for playing this guessing game with me, ${userName}! You got ${correctCount} out of 5 correct!`);
 
 //if (q5 === 'Y' || q5 === 'YES'){
 //  console.log('Si, es la verdad! It\'s my favorite!' + ' Thank you for playing this guessing game with me, ' + userName + '!');
